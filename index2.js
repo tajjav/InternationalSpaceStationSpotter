05bf617ab8b0b4efabd6142ec8efe9b94f969464
tajjav <taj.jav@hotmail.com>
2023-10-25 13:55:29 -0600

    made fetchMyIP and fetchCoordsByIP functions promised version

---
+++ index2.js
@@ -0,0 +1,9 @@
+const {fetchMyIP, fetchCoordsByIP} = require("./iss_promised");
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then((body) => {
+    console.log(body);
+  })
+  
+
---
+++ iss_promised.js
@@ -0,0 +1,23 @@
+const request = require("request-promise-native");
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function () {
+  return request("https://api.ipify.org?format=json");
+};
+
+
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`)
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
